b683cc0d1b89e99b79d4dec68413c2604f215e89
siuser小伟 <siuserxiaowei@192.168.0.220>
2026-05-16 21:42:15 +0800

    feat: 主页表格 + 筛选/搜索/风险徽章/链接跳转

---
+++ src/components/ChannelForm.tsx
@@ -0,0 +1,8 @@
+import type { Channel } from "../lib/channels";
+export default function ChannelForm(_: {
+  channel: Channel | null;
+  onClose: () => void;
+  onSaved: () => void;
+}) {
+  return null;
+}
